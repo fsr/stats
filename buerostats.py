e1e59927b76142c4f585464c58c6c4a05967de53
Felix Wittwer <felix.wittwer@gmail.com>
2016-03-22 14:21:56 +0100

    rudimentary plotting

--- buerostats.py
+++ buerostats.py
@@ -35,8 +35,35 @@ def main():
     for row in rows:
         date = time.localtime(row[1])
         data[date[6]][date[3]][date[4]].append(row[2])
+
+    avg_data = {
+        day_k: {
+            hour_k: {
+                min_k: sum(min_v)/len(min_v)
+                for min_k, min_v in hour_v.items()
+            }
+            for hour_k, hour_v in day_v.items()
+        }
+        for day_k, day_v in data.items()
+    }
+
     with open('results.json', 'w') as f:
-        json.dump(data, f, indent=4)
+        json.dump(avg_data, f, indent=4)
+
+    all_x = []
+    cnt = 0
+    for day in avg_data:
+        for hour in avg_data[day]:
+            for minute in avg_data[day][hour]:
+                all_x.append(avg_data[day][hour][minute])
+
+    import matplotlib.pyplot as plt
+    plt.plot(all_x)
+    plt.ylabel('Lichtsensor Output')
+    plt.xlabel('Zeit')
+    plt.axis([0, 10080, 0, 1000])
+    plt.savefig('test.png')
+    plt.show()
 
 
 if __name__ == '__main__':
