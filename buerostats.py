cc4387c76b9886d91fdb8938600919fa09959d28
Felix Wittwer <felix.wittwer@gmail.com>
2016-03-27 20:07:33 +0200

    Rudimentary csv output for Punchcard

--- buerostats.py
+++ buerostats.py
@@ -1,13 +1,15 @@
 import sqlite3
 import time
 import json
+import sys
+import csv
 
 
 def get_raw_data():
     '''Fetches all rows from the buerostatus database'''
     connection = sqlite3.connect('buerostatus.db')
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM buerostatus")
+    cursor.execute("SELECT * FROM buerostatus")  # ORDER BY ts?
     rows = cursor.fetchall()
     connection.close()
 
@@ -27,7 +29,27 @@ def init_data():
     return data
 
 
+def graph_draw(avg_data):
+    all_x = []
+    cnt = 0
+    for day in avg_data:
+        for hour in avg_data[day]:
+            for minute in avg_data[day][hour]:
+                all_x.append(avg_data[day][hour][minute])
+
+    import matplotlib.pyplot as plt
+    plt.plot(all_x)
+    plt.ylabel('Lichtsensor Output')
+    plt.xlabel('Zeit')
+    plt.axis([0, 10080, 0, 1000])
+    plt.savefig('test.png')
+    plt.show()
+
+
 def main():
+    args = sys.argv
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+            'Saturday', 'Sunday']
     rows = get_raw_data()
 
     data = init_data()
@@ -47,23 +69,37 @@ def main():
         for day_k, day_v in data.items()
     }
 
-    with open('results.json', 'w') as f:
+    with open('results.old.json', 'w') as f:
         json.dump(avg_data, f, indent=4)
 
-    all_x = []
-    cnt = 0
-    for day in avg_data:
-        for hour in avg_data[day]:
-            for minute in avg_data[day][hour]:
-                all_x.append(avg_data[day][hour][minute])
+    new_avg_data = {
+        day_k: {
+            hour_k: round(sum(hour_v.values())/len(hour_v.values()))
+            for hour_k, hour_v in day_v.items()
+        }
+        for day_k, day_v in avg_data.items()
+    }
 
-    import matplotlib.pyplot as plt
-    plt.plot(all_x)
-    plt.ylabel('Lichtsensor Output')
-    plt.xlabel('Zeit')
-    plt.axis([0, 10080, 0, 1000])
-    plt.savefig('test.png')
-    plt.show()
+    with open('results.json', 'w') as f:
+        json.dump(new_avg_data, f, indent=4)
+
+    if len(args) > 1 and args[1] == 'draw':
+        graph_draw(avg_data)
+
+    csvlist = [',0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23']
+    for day_id in new_avg_data:
+        val_str = days[int(day_id)]
+        vals = []
+        for i in range(0, 24):
+            vals.append(str(new_avg_data[day_id][i]))
+        val_str = val_str + ',' + ','.join(vals)
+        csvlist.append(val_str)
+
+    # print(csvlist)
+
+    with open('some.csv', 'w', newline='') as f:
+        for line in csvlist:
+            f.write(line + '\n')
 
 
 if __name__ == '__main__':
